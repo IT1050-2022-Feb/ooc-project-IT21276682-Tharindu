e3aa587f854e772e400ed574c43f4fedadcce43a
IT21276682-Tharindu <99549447+IT21276682-Tharindu@users.noreply.github.com>
2022-05-20 17:30:42 +0530

    Create Driver.h

---
+++ Driver.h
@@ -0,0 +1,18 @@
+#include "Employee.h"
+class Driver : public Employee
+{
+protected:
+	char Driver_Category[50];
+	int Driver_ID;
+	int contactNo;
+	char Assigned_Vehicle[50];
+
+public:
+	Driver(const char Employee_User_Name[], const char DriverCategory[], int DriverID, int Driver_contactNo, const char Employee_Address[], const char AssignedVehicle[]);
+	void Review_Employee_Reports(const char* reports);
+	void setRideUserDetails(const char* add, int cNo, const char* uEmail);
+	void updateRideUserDetails(const char* add, int cNo, const char* uEmail);
+	void displayRideUserDetails(const char* add, int cNo, const char* uEmail);
+	~Driver();
+};
+
