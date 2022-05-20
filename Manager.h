f605dce6fadef8e921b003c1182ea99e47811cb0
IT21276682-Tharindu <99549447+IT21276682-Tharindu@users.noreply.github.com>
2022-05-20 17:15:27 +0530

    Create Manager.h

---
+++ Manager.h
@@ -0,0 +1,21 @@
+#include "Employee.h"
+class Manager : public Employee
+{
+protected:
+	char Manager_Division[50];
+	int Manager_ID;
+	int contactNo;
+
+public:
+	Manager(const char Employee_User_Name[], int EmployeeID, const char ManagerDivision[], int ManagerID, const char Employee_Address[], int Employee_contactNo, const char Employee_email[]);
+	void Manage_User_Accounts();
+	void approve_payment();
+	void Generating_reports();
+	void Monitoring_Vehicle_Activity();
+	void Review_reports(const char* reports);
+	void Manage_User_Details(int j_id);
+	void Manage_Company_Details(const char* c_name, int c_id);
+	void Manage_Resumes(const char* resumeName);
+	~Manager();
+};
+
