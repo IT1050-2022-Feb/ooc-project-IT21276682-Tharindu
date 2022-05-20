be5e6e312825ace2a74a87be811da799f5ade2b1
IT21276682-Tharindu <99549447+IT21276682-Tharindu@users.noreply.github.com>
2022-05-20 17:11:18 +0530

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,1 +1,23 @@
+#include "Employee.h"
+class Admin : public Employee
+{
+protected:
+	char Admin_Division[50];
+	int Admin_ID;
+	int contactNo;
+
+	
+
+public:
+	Admin(const char Employee_User_Name[], int EmployeeID, const char AdminDivision[], int AdminID, const char Employee_Address[], int Employee_contactNo, const char Employee_email[]);
+	void Manage_User_Accounts();
+	void Document_Requirements();
+	void store_reports(const char* reports);
+	void Manage_User_Info(int js_id);
+	void Manage_User_Details(int j_id);
+	void Manage_Company_Info(const char* c_name, int c_id);
+	void Manage_Resumes(const char* resumeName);
+	~Admin();
+
+};
 
