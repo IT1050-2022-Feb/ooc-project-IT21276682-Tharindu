b988dc8cfd7d3d53e9bbd63faedbc109c3a3baf0
IT21276682-Tharindu <99549447+IT21276682-Tharindu@users.noreply.github.com>
2022-05-20 17:25:59 +0530

    Create Driver.cpp

---
+++ Driver.cpp
@@ -0,0 +1,36 @@
+#include "Driver.h"
+#include <cstring>
+
+//constructor with parameters
+
+Driver::Driver(const char Employee_User_Name[], const char DriverCategory[], int DriverID, int Driver_contactNo, const char Employee_Address[], const char AssignedVehicle[])
+
+{
+	strcpy(Driver_Category, DriverCategory);
+	Driver_ID = DriverID;
+	contactNo = Driver_contactNo;
+	strcpy(Assigned_Vehicle, AssignedVehicle);
+}
+
+void Driver::Review_Employee_Reports(const char* reports)
+{
+}
+
+void Driver::setRideUserDetails(const char* add, int cNo, const char* uEmail)
+{
+}
+
+void Driver::updateRideUserDetails(const char* add, int cNo, const char* uEmail)
+{
+}
+
+void Driver::displayRideUserDetails(const char* add, int cNo, const char* uEmail)
+{
+}
+
+Driver::~Driver()
+{
+}
+
+
+
