6c5ba14de3ef379346f4edc36205507dad63eb73
NupeArachchigePGN <99308070+NupeArachchigePGN@users.noreply.github.com>
2022-05-20 17:51:51 +0530

    Create User.h

---
+++ User.h
@@ -0,0 +1,17 @@
+class User
+{
+protected:
+	char Name[50];
+	int User_ID;
+	char address[50];
+	int contactNo;
+	char email[50];
+public:
+	User();
+	User(const char uName[], int uRegID, const char* u_address, int u_contactNo, const char* u_email);
+	void setRideUserDetails(char add, int cNo, char uEmail);
+	virtual void updateRideUserDetails();
+	virtual void displayRideUserDetails();
+	~User();
+};
+
