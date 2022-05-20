4fbfc24aac42fafd0a7ebf6f4f4da9c468448d1e
it21276996 <99540008+it21276996@users.noreply.github.com>
2022-05-20 17:09:25 +0530

    Create payment.h

---
+++ payment.h
@@ -0,0 +1,19 @@
+#pragma once
+class payment
+{
+protected:
+    int payemnt_Id;
+    double Total;
+    char card_Type[4];
+    char user_email[50];
+    char card_Holder[50];
+
+public:
+    payment(int id, double total, const char c_type[], const char email[], const char cardHolder[]);
+    double set_total(double total);
+    double get_total();
+    double display_payment();
+    ~payment();
+
+
+};
