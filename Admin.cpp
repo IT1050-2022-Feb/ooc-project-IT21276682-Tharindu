c5227de1f99807b6b3fb76fa26ba96e0ad0c584a
IT21276682-Tharindu <99549447+IT21276682-Tharindu@users.noreply.github.com>
2022-05-20 15:28:30 +0530

    Create Admin.cpp

---
+++ Admin.cpp
@@ -0,0 +1,46 @@
+#include "Admin.h"
+#include <cstring>
+
+//constructor with parameters
+
+Admin::Admin(const char Employee_User_Name[], int EmployeeID, const char AdminDivision[], int AdminID, const char Employee_Address[], int Employee_contactNo, const char Employee_email[])
+{
+	strcpy(Admin_Division, AdminDivision);
+	strcpy(Employee_Name, Employee_User_Name);
+	Admin_ID = AdminID;
+	Employee_ID = EmployeeID;
+	contactNo = Employee_contactNo;
+	strcpy(address, Employee_Address);
+	strcpy(email, Employee_email);
+}
+void Admin::Manage_User_Accounts()
+{
+}
+
+void Admin::Document_Requirements()
+{
+}
+
+void Admin::store_reports(const char* reports)
+{
+}
+
+void Admin::Manage_User_Info(int js_id)
+{
+}
+
+void Admin::Manage_User_Details(int j_id)
+{
+}
+
+void Admin::Manage_Company_Info(const char* c_name, int c_id)
+{
+}
+
+void Admin::Manage_Resumes(const char* resumeName)
+{
+}
+
+Admin::~Admin()
+{
+}
